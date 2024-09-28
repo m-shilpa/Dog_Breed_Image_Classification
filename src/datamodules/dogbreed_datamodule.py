80f4422bbb310568423acc38c911a5548e9f0f31
Shilpa M <goldenflowers21@gmail.com>
2024-09-28 19:53:11 +0000

    code for dog breed classifier

---
+++ src/datamodules/dogbreed_datamodule.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+from typing import Union
+import zipfile
+from random import shuffle
+
+import lightning as L
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torchvision.datasets.utils import download_and_extract_archive
+from torch.utils.data import Subset
+
+class DogBreedImageDataModule(L.LightningDataModule):
+    def __init__(self, dl_path: Union[str, Path] = "data", num_workers: int = 0, batch_size: int = 8):
+        super().__init__()
+        self._dl_path = dl_path
+        self._num_workers = num_workers
+        self._batch_size = batch_size
+
+    def prepare_data(self):
+        """Download images and prepare images datasets."""
+        with zipfile.ZipFile('dog_breed_image_dataset.zip', 'r') as zip_ref:
+            zip_ref.extractall(self._dl_path)
+
+    @property
+    def data_path(self):
+        return Path(self._dl_path).joinpath("dataset")
+
+    @property
+    def normalize_transform(self):
+        return transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    @property
+    def train_transform(self):
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            self.normalize_transform,
+        ])
+
+    @property
+    def valid_transform(self):
+        return transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            self.normalize_transform
+        ])
+
+    def create_dataset(self, root, transform):
+        return ImageFolder(root=root, transform=transform)
+
+    def __dataloader(self, train: bool):
+        """Train/validation/test loaders."""
+        split_size =0.8
+        full_dataset = self.create_dataset(self.data_path, self.train_transform)
+
+        # indexes = list(range(len(full_dataset)))
+        # shuffle(indexes)
+        # indexes_train = indexes[:int(len(full_dataset)*split_size)]
+        # indexes_test = indexes[int(len(full_dataset)*split_size):]
+
+        indexes_train = range(int(len(full_dataset)*split_size))
+        indexes_test = range(int(len(full_dataset)*split_size), len(full_dataset))
+
+ 
+        if train:
+            dataset = Subset(full_dataset, indexes_train)
+        else:
+            dataset = Subset(full_dataset, indexes_test)
+        return DataLoader(dataset=dataset, batch_size=self._batch_size, num_workers=self._num_workers, shuffle=train)
+
+    def train_dataloader(self):
+        return self.__dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.__dataloader(train=False)
+
+    def test_dataloader(self):
+        return self.__dataloader(train=False)  # Using validation dataset for testing
---
+++ src/train.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint, RichProgressBar, RichModelSummary
+from lightning.pytorch.loggers import TensorBoardLogger
+
+from datamodules.dogbreed_datamodule import DogBreedImageDataModule
+from models.dogbreed_classifier import DogBreedClassifier
+from utils.logging_utils import setup_logger, task_wrapper
+
+@task_wrapper
+def train_and_test(data_module, model, trainer):
+    trainer.fit(model, data_module)
+    trainer.validate(model, data_module)
+
+def main():
+    # Set up paths
+    base_dir = Path('.')
+    data_dir = base_dir / "data"
+    log_dir = base_dir / "logs"
+    
+    # Set up logger
+    setup_logger(log_dir / "train_log.log")
+
+    # Initialize DataModule
+    data_module = DogBreedImageDataModule(dl_path=data_dir, batch_size=32, num_workers=0)
+
+    # Initialize Model
+    model = DogBreedClassifier(lr=1e-3)
+
+    # Set up checkpoint callback
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=log_dir / "dogbreed_classification" / "checkpoints",
+        filename="epoch={epoch:02d}-val_loss={val_loss:.2f}",
+        save_top_k=3,
+        monitor="val_loss",
+        mode="min",
+    )
+
+    # Initialize Trainer
+    trainer = L.Trainer(
+        max_epochs=5,
+        callbacks=[
+            checkpoint_callback,
+            RichProgressBar(),
+            RichModelSummary(max_depth=2)
+        ],
+        accelerator="auto",
+        logger=TensorBoardLogger(save_dir=log_dir, name="dogbreed_classification",
+        ),
+    )
+
+    # Train and test the model
+    train_and_test(data_module, model, trainer)
+
+if __name__ == "__main__":
+    main()
